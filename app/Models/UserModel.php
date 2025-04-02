9c8dbfde799c8e41038c102d5f83c75049e3183c
Tien-Quyet-12225 <quyetdtpn00073@gmail.com>
2025-04-02 22:10:10 +0700

    Update UserModel.php

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,3 +6,15 @@ class UserModel extends BaseModel{
     public function getUserByEmail($email){
         return $this->findByColumn('users', 'email', $email);
     }
+    public function add($username, $fullname, $email, $password, $avatar){
+        $data = [
+            'username' => $username,
+            'full_name' => $fullname,
+            'email' => $email,
+            'password_hash' => $password,
+            'avatar' => $avatar,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ];
+        return $this->create('users', $data);
+    }
