0115756dd7c5cfb7c8841a622768db6232ba3d9d
hoanphandang <phandanghoan.yt@gmail.com>
2025-04-06 12:00:16 +0700

    Update CategoryAdminModel.php

--- app/Models/admin/CategoryAdminModel.php
+++ app/Models/admin/CategoryAdminModel.php
@@ -11,6 +11,24 @@ class CategoryAdminModel extends BaseModel{
         return $this->findAll($this->table);
     }
 
-    
+    public function del($id){
+        return $this->delete($this->table, $id);
+    }
+
+    public function add($name, $description){
+        $data = [
+            'name' => $name,
+            'description' => $description
+        ];
+        return $this->create($this->table,$data);
+    }
+
+    public function updt($id, $name, $description){
+        $data = [
+            'name' => $name,
+            'description' => $description
+        ];
+        return $this->update($this->table, $id, $data);
+    }
 }
 ?>
