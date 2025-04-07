010d3d3ec001f0928d459dfc842fa300eb76d043
Tien-Quyet-12225 <quyetdtpn00073@gmail.com>
2025-04-07 20:31:07 +0700

    Update ArticleAdminModel.php

--- app/Models/admin/ArticleAdminModel.php
+++ app/Models/admin/ArticleAdminModel.php
@@ -6,10 +6,10 @@ use App\Models\BaseModel;
 
 class ArticleAdminModel extends BaseModel
 {
-  protected $table = 'articles';
-  public function list()
-  {
-    $sql = "SELECT a.id, a.title, a.image,
+    protected $table = 'articles';
+    public function list()
+    {
+        $sql = "SELECT a.id, a.title, a.image,
                         u.username, c.name as category 
                 FROM articles a 
                 JOIN users u ON a.author_id = u.id 
@@ -18,35 +18,75 @@ class ArticleAdminModel extends BaseModel
                 LEFT JOIN comments com ON a.id = com.article_id 
                 GROUP BY a.id, a.title, a.image, u.username, c.name
                 ORDER BY a.id DESC";
-    return $this->query($sql);
-  }
+        return $this->query($sql);
+    }
 
-  public function add($title, $content, $category_id, $author_id, $image)
-  {
-    $data = [
-      'title' => $title,
-      'content' => $content,
-      'category_id' => $category_id,
-      'author_id' => $author_id,
-      'image' => $image
-    ];
-    return $this->create($this->table, $data, true);
-  }
+    public function add($title, $content, $author_id, $category_id, $image)
+    {
+        $data = [
+            'title' => $title,
+            'content' => $content,
+            'author_id' => $author_id,
+            'category_id' => $category_id,
+            'image' => $image
+        ];
 
-  public function del($article_id)
-  {
-    return $this->delete($this->table, $article_id);
-  }
+        return $this->create($this->table, $data, true);
+    }
 
-  public function updt($article_id, $title, $content, $category_id, $image)
-  {
-    $data = [
-      'title' => $title,
-      'content' => $content,
-      'category_id' => $category_id,
-      'image' => $image
-    ];
+    public function del($article_id)
+    {
+        return $this->delete($this->table, $article_id);
+    }
 
-    return $this->update($this->table, $article_id, $data);
-  }
+    public function getArtById($article_id)
+    {
+        $sql = "SELECT a.*, c.name as category_name, u.username
+                FROM articles a
+                JOIN users u ON a.author_id = u.id
+                LEFT JOIN categories c ON a.category_id = c.id
+                WHERE a.id =  :id";
+        $params = [
+            ':id' => $article_id
+        ]; 
+        return $this->query($sql, $params, false);       
+    }
+    public function add_image($file_name, $file_path){
+        $data = [
+            'file_name' => $file_name,
+            'file_path' => $file_path
+        ];
+        return $this->create('images', $data, true);
+    }
+
+    public function add_article_image($article_id, $image_id){
+        $data = [
+            'article_id' => $article_id,
+            'image_id' => $image_id
+        ];
+        return $this->create('article_images', $data);
+    }
+
+    public function getArtImg($article_id){
+        $sql = "SELECT i.*
+                FROM article_images ai
+                JOIN images i ON ai.image_id = i.id
+                WHERE ai.article_id = :article_id";
+        $params = [
+            ':article_id' => $article_id
+        ];
+        return $this->query($sql, $params);
+    }
+
+
+    public function updt($article_id, $title, $content, $category_id, $image){
+        $data = [
+            'title' => $title,
+            'content' => $content,
+            'category_id' => $category_id,
+            'image' => $image
+        ];
+
+        return $this->update($this->table, $article_id, $data);
+    }
 }
