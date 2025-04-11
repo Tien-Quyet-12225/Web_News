d182de69ee4ef7f413ea5ecc03fd8761eb80a108
Tien-Quyet-12225 <quyetdtpn00073@gmail.com>
2025-04-11 17:57:55 +0700

    Update letter_tag.blade.php

--- src/views/layouts/home/letter_tag.blade.php
+++ src/views/layouts/home/letter_tag.blade.php
@@ -7,7 +7,7 @@
         <div class="input-group mb-2" style="width: 100%;">
             <input type="text" class="form-control form-control-lg" placeholder="Your Email">
             <div class="input-group-append">
-                <button class="btn btn-primary font-weight-bold px-3">Sign Up</button>
+                <a href="http://web_news.test/show_register" class="btn btn-primary font-weight-bold px-3">Sign Up</a>
             </div>
         </div>
         <small>Lorem ipsum dolor sit amet elit</small>
@@ -18,20 +18,13 @@
 <!-- Tags Start -->
 <div class="mb-3">
     <div class="section-title mb-0">
-        <h4 class="m-0 text-uppercase font-weight-bold">Tags</h4>
+        <h4 class="m-0 text-uppercase font-weight-bold">Categories</h4>
     </div>
     <div class="bg-white border border-top-0 p-3">
         <div class="d-flex flex-wrap m-n1">
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Politics</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Business</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Corporate</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Business</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Health</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Education</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Science</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Business</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Foods</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Travel</a>
+            @foreach ($categories as $category)
+                <a href="{{ BASE_URL }}category/{{ $category['id'] }}" class="btn btn-sm btn-outline-secondary m-1">{{ $category['name'] }}</a>
+            @endforeach
         </div>
     </div>
 </div>
