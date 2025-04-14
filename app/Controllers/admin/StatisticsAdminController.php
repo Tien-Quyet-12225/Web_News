89847c5b1bc825aa54ba5dc702f6a74c1af1e348
Tien-Quyet-12225 <quyetdtpn00073@gmail.com>
2025-04-14 12:20:15 +0700

    Thống kê,báo cáo
    Thống kê,báo cáo

---
+++ app/Controllers/admin/StatisticsAdminController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+use App\Models\Admin\StatisticsAdminModel;
+
+class StatisticsAdminController extends BaseController
+{
+    protected $statisticsModel;
+
+    public function __construct()
+    {
+        $this->statisticsModel = new StatisticsAdminModel();
+    }
+
+    public function index()
+    {
+        // Lấy dữ liệu thống kê
+        $totals = $this->statisticsModel->getTotals();
+        $categoryStats = $this->statisticsModel->getCategoryStats();
+        $articleStats = $this->statisticsModel->getArticleStats();
+
+        // Hiển thị view với dữ liệu
+        $this->render('admin.dashboard.statistics', compact('totals', 'categoryStats', 'articleStats'));
+    }
+} 
